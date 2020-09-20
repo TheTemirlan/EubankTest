00380acb01178cd2d5867dae21b0634fac101fe8
Temirlan <temirlan.zholbolat@gmail.com>
2020-09-21 02:15:47 +0500

    EubankTest project added.

---
+++ EubankTest/TestAPI/Controllers/FooBarController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FooBarController : ControllerBase
+    {
+        // GET api/foobar/<number>
+        [HttpGet("{number}")]
+        public IActionResult Get(int number)
+        {
+            string result;
+            if (number % 3 == 0 && number % 5 == 0)
+                result = "foobar";
+            else if (number % 3 == 0)
+                result = "foo";
+            else if (number % 5 == 0)
+                result = "bar";
+            else
+                return NotFound();
+
+            return new ObjectResult(result);
+        }
+    }
+}
